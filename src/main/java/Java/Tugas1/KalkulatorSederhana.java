26df2094a5a02322015861840be508aea497f20a
CibiBumbum <kariralfian@gmail.com>
2023-10-31 11:32:19 +0700

    Coba Aja

--- src/main/java/Java/Tugas1/KalkulatorSederhana.java
+++ src/main/java/Java/Tugas1/KalkulatorSederhana.java
@@ -1,48 +1,55 @@
 package Java.Tugas1;
+
+import jdk.internal.org.objectweb.asm.tree.TableSwitchInsnNode;
+
 import java.util.Scanner;
+
+import static java.lang.System.out;
+
 public class KalkulatorSederhana {
     public static void main(String[] args) { //menggunakan shortcut PSVM.
-        Scanner input = new Scanner(System.in); //saya menambahkan Live Template 'tai' (This as Input) untuk membuat Scanner baru.
+        Scanner input = new Scanner(System.in);
+        out.println("Halo Ini cuma coba");
 
-        System.out.println("=== Kalkulator Sederhana ===");
-        System.out.println("Pilihlah operasi yang ingin anda gunakan :");
-        System.out.println("1. Penjumlahan");
-        System.out.println("2. Pengurangan");
-        System.out.println("3. Perkalian");
-        System.out.println("4. Pembagian");
+        out.println("=== Kalkulator Sederhana ===");
+        out.println("Pilihlah operasi yang ingin anda gunakan :");
+        out.println("1. Penjumlahan");
+        out.println("2. Pengurangan");
+        out.println("3. Perkalian");
+        out.println("4. Pembagian");
 
-        System.out.println("Masukkan pilihan (1/2/3/4) : ");
+        out.println("Masukkan pilihan (1/2/3/4) : ");
         int pilihan = input.nextInt();
 
-        System.out.println("Masukkan bilangan pertama : ");
+        out.println("Masukkan bilangan pertama : ");
         double bilangan1 = input.nextDouble();
 
-        System.out.println("Masukkan bilangan kedua : ");
+        out.println("Masukkan bilangan kedua : ");
         double bilangan2 = input.nextDouble();
 
         double hasil = 0;
 
         switch (pilihan) {
-            case 1 :
+            case 1:
                 hasil = bilangan1 + bilangan2; //Live template (tambah)
                 break;
-            case 2 :
+            case 2:
                 hasil = bilangan1 - bilangan2; //Live template (kurang)
                 break;
-            case 3 :
+            case 3:
                 hasil = bilangan1 * bilangan2; //Live template (kali)
                 break;
-            case 4 :
-                if(bilangan2 != 0){
+            case 4:
+                if (bilangan2 != 0) {
                     hasil = bilangan1 / bilangan2;
                 } else {
-                    System.out.println("Tidak bisa melakukan pembagian dengan nol.");
+                    out.println("Tidak bisa melakukan pembagian dengan nol.");
                     System.exit(1);
                 }
-            default :
-                System.out.println("Pilihan tidak valid.");
+            default:
+                out.println("Pilihan tidak valid.");
                 System.exit(1);
         }
-        System.out.println("Hasil : " + hasil);
+        out.println("Hasil : " + hasil);
     }
 }
